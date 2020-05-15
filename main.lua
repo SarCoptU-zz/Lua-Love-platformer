b1797ab4b78f86ea70cdf7cf810395747c2945f6
Remus Sferle <remussferle@gmail.com>
2020-05-15 05:06:39 +0100

    added player movement

--- main.lua
+++ main.lua
@@ -1,20 +1,30 @@
 -- (1) basic rectangle drawing
+-- (2) added player movement with love.keyboard.isDown('key') in love.update(dt)
+--     player / enemy x position are now variables
 
 WINDOW_WIDTH = love.graphics.getWidth()
 WINDOW_HEIGHT = love.graphics.getHeight()
 
 function love.load()
-    
+    playerX = 200
+    enemyX = 500
 end
 
 function love.update(dt)
+    dx = 300 * dt
 
+    -- player movement
+    if love.keyboard.isDown('left') then
+        playerX = playerX - dx
+    elseif love.keyboard.isDown('right') then
+        playerX = playerX + dx
+    end
 end
 
 function love.draw()
     ground = love.graphics.line(0, WINDOW_HEIGHT - 100, WINDOW_WIDTH, WINDOW_HEIGHT - 100)
-    player = love.graphics.rectangle('line', 200, WINDOW_HEIGHT - 202, 100, 100)
-    enemy = love.graphics.rectangle('line', 500, WINDOW_HEIGHT - 252, 100, 150)
+    player = love.graphics.rectangle('line', playerX, WINDOW_HEIGHT - 202, 100, 100)
+    enemy = love.graphics.rectangle('line', enemyX, WINDOW_HEIGHT - 252, 100, 150)
     -- print window width and height
     love.graphics.print(WINDOW_WIDTH, 100, 100)
     love.graphics.print(WINDOW_HEIGHT, 200, 100)
